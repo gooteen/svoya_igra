7f06cd6a6182323ab2c795a49bb93555539b840c
gooteen <108371872+gooteen@users.noreply.github.com>
2023-11-09 19:39:23 +0300

    experiment

---
+++ Assets/TestScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        ReadString();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReadString()
+    {
+        string path = Application.dataPath + "/test.txt";
+        if (File.Exists(path))
+        {
+            StreamReader reader = new StreamReader(path);
+            string var = reader.ReadToEnd();
+            Debug.Log(var);
+            text.text = var;
+            reader.Close();
+        } else
+        {
+            Debug.Log("Fuck");
+        }
+
+    }
+}
